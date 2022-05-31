65297ead885808ed63d0bb91bff79feb7fb635a6
Tyler Ryden <tryden11@gmail.com>
2022-05-31 11:18:59 -0500

    initialized the buttons, editext, and listview

--- app/src/main/java/com/tryden12/weatherrestapi/MainActivity.java
+++ app/src/main/java/com/tryden12/weatherrestapi/MainActivity.java
@@ -3,12 +3,51 @@ package com.tryden12.weatherrestapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // assign values to each control in layout
+        btn_cityID = findViewById(R.id.btn_getCityByID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReports);
+
+        // click listeners
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked me 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked me 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked me 3", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
